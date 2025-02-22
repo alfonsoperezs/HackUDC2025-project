571425981d4e5dffa20a411ea0faa49d981b0d10
alfonsoperezs <fonpesan@gmail.com>
2025-02-22 15:34:17 +0100

    implementado objeto Task

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,5 @@
+import json
+from datetime import datetime
 
 class Task:
     def __init__(self, id, name, start_date, end_date):
@@ -7,6 +9,16 @@ class Task:
         self.end_date = end_date
 
     def to_json(self):
-        pass
+        return json.dumps({
+            "id": self.id,
+            "name": self.name,
+            "start_date": self.start_date.strftime("%Y-%m-%d") if self.start_date else None,
+            "end_date": self.end_date.strftime("%Y-%m-%d") if self.end_date else None
+        })
+
+    @classmethod
+    def from_json(cls, json_str):
+        data = json.loads(json_str)
+        return cls(**data)
 
         
