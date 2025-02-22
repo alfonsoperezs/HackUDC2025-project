a6656d1e5352304c8501cae71256d6ee3c1fa1f9
su_v_allal <vilalsus@gmail.com>
2025-02-22 02:07:10 +0100

    tabla user

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,10 @@
-import flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False)
+    lastname = db.Column(db.String(100), nullable = False)
+    github = db.Column(db.String(100), nullable = False)
+
